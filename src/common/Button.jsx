5ddd6a98331d27fb5fe6ddc33c80d7452dd62c35
zetra <karanpatadia28@gmail.com>
2024-11-27 20:10:22 +0530

    mobile fix

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,7 +1,13 @@
 const Button = ({ children, onClick }) => {
+  const handleTouch = (e) => {
+    e.preventDefault();
+    onClick(e);
+  };
+
   return (
     <button
       onClick={onClick}
+      onTouchStart={handleTouch}
       className={`ml-2 h-full w-fit appearance-none whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner`}
     >
       {children}
