7cd69e9b8bd9df021f225ef0aaa6954e02a0c27e
zetra <karanpatadia28@gmail.com>
2024-11-27 20:16:57 +0530

    adding cursor pointer css

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ children, onClick }) => {
     <button
       onClick={onClick}
       onTouchStart={handleTouch} // Add touch event handler
-      className="ml-2 h-full w-fit whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner"
+      className="ml-2 h-full w-fit cursor-pointer whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner"
     >
       {children}
     </button>
