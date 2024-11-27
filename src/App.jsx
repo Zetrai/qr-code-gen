4301dcc9e4dd13d184d658557d83ad74b8a05705
zetra <karanpatadia28@gmail.com>
2024-11-27 16:50:14 +0530

    Changed design

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import Button from './common/Button';
 
 const App = () => {
   return (
-    <div className="bg-darkBG flex-center relative h-screen w-screen flex-col">
+    <div className="bg-darkBG relative flex h-screen w-screen flex-col items-center py-12">
       <BgIllustration className="absolute right-[-200px] top-1/4 h-3/5" />
 
-      <h1 className="text-darkText mb-6 text-center font-saira text-2xl">
-        <LogoQRGen />
-      </h1>
-      <div className="bg-darkInput flex-center z-1 relative mx-7 h-14 w-[90%] rounded-[10px] border-2 border-primary p-2">
-        <input
-          className="font-outfit z-100 text-darkText w-full bg-transparent focus:border-transparent focus:outline-none"
-          placeholder="Enter Url Here..."
-        />
-        <Button />
-      </div>
+      <section className="flex-center h-[20%] w-full flex-col">
+        <h1 className="text-darkText mb-6 text-center font-saira text-2xl">
+          <LogoQRGen />
+        </h1>
+        <div className="bg-darkInput flex-center z-1 relative mx-7 h-14 w-[90%] rounded-[10px] border-2 border-primary p-2">
+          <input
+            className="font-outfit z-100 text-darkText w-full bg-transparent focus:border-transparent focus:outline-none"
+            placeholder="Enter Url Here..."
+          />
+          <Button>QR Code</Button>
+        </div>
+      </section>
+
+      <section className="flex h-[80%] w-full flex-col items-center gap-2 px-2 pt-10">
+        <h1 className="font-saira text-3xl text-primary">QR</h1>
+        <div className="h-[300px] w-[300px] rounded-[10px] border-2 border-primary"></div>
+        <div className="h-16 py-2">
+          <Button>Download</Button>
+        </div>
+      </section>
     </div>
   );
 };
--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,7 +1,9 @@
-const Button = () => {
+const Button = ({ children, className }) => {
   return (
-    <button className="text-darkText w-fitasd ml-2 h-full whitespace-nowrap rounded-[10px] bg-primary px-4 text-center">
-      QR Code
+    <button
+      className={`text-darkText w-fitasd ml-2 h-full whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat ${className}`}
+    >
+      {children}
     </button>
   );
 };
