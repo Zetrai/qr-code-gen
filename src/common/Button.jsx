7a5d52528d403c5308dda4d0426401b3898e4f6c
zetra <karanpatadia28@gmail.com>
2024-11-27 20:12:52 +0530

    try fix1

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -8,10 +8,9 @@ const Button = ({ children, onClick }) => {
     <button
       onClick={onClick}
       onTouchStart={handleTouch}
-      className={`ml-2 h-full w-fit appearance-none whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner`}
+      className="ml-2 h-full w-fit whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner"
     >
       {children}
     </button>
   );
 };
-export default Button;
