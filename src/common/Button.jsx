142fcaa5d022a2353d10753744fded587c4cbcdb
zetra <karanpatadia28@gmail.com>
2024-11-27 20:14:31 +0530

    fix export btn

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -7,10 +7,12 @@ const Button = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      onTouchStart={handleTouch}
+      onTouchStart={handleTouch} // Add touch event handler
       className="ml-2 h-full w-fit whitespace-nowrap rounded-[10px] bg-primary px-4 text-center font-montserrat text-darkText shadow-md transition-transform duration-150 active:scale-95 active:shadow-inner"
     >
       {children}
     </button>
   );
 };
+
+export default Button;
